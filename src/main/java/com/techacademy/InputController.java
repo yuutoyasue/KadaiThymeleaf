de4094a6a0f787c56037311b43e64990fcf3423c
yuutoyasue <uk27fbk@yahoo.co.jp>
2023-02-28 20:42:32 +0900

    課題

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,15 +1,18 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
 
     @GetMapping("/input")
-    public String getInput() {
+    public String getInput(@RequestParam(name ="previous", required = false) String previous, Model model) {
+        model.addAttribute("previous", previous);
         // input.htmlに画面遷移
         return "input";
-
     }
+
 }
